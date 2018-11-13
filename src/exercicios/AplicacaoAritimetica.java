80bdbce952af7545e74bdc79dd96813e533d3b3c
Gilberto Giudice <gilbertogiudice37@gmail.com>
2018-11-13 21:26:36 -0200

    Exercício corrigido

--- src/exercicios/AplicacaoAritimetica.java
+++ src/exercicios/AplicacaoAritimetica.java
@@ -13,22 +13,37 @@ import javax.swing.JOptionPane;
  */
 public class AplicacaoAritimetica {
     public static void main(String[] args) {
-        int soma,multiplicar,subtrair,dividir, num1 = 0, num2 = 0;
-        
-        num1 = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite um numero: "));
-        num2 = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite um numero: "));
-
-        soma = num1 + num2;
-        multiplicar= num1 * num2;
-        dividir= num1 / num2;
-        subtrair= num1 - num2;
-        
-        JOptionPane.showMessageDialog(null, "A soma é: " + subtrair);
-        JOptionPane.showMessageDialog(null, "A soma é: " + dividir);
-        JOptionPane.showMessageDialog(null, "A soma é: " + multiplicar);
-        JOptionPane.showMessageDialog(null, "A soma é: " + soma);
-        
-        
-    }
+    int opc=0;
+    double valor1, valor2, resultado;
     
-}
+    while(opc!=5){
+        
+       opc=Integer.parseInt(JOptionPane.showInputDialog("Digite uma opção: \n 1 - soma\n 2 - subtração\n 3 - dividir \n 4 - multiplicação \n 5 - sair",JOptionPane.INFORMATION_MESSAGE));
+        if (opc==5){
+            System.exit(0);
+        }
+       
+       
+       
+       valor1=Double.parseDouble(JOptionPane.showInputDialog("Digite o valor1: "));
+       valor2=Double.parseDouble(JOptionPane.showInputDialog("Digite o valor2: "));
+        
+        if (opc==1){
+            resultado= valor1+valor2;
+            JOptionPane.showMessageDialog(null,"O valor da soma é: "+resultado+" RESULTADO DA SOMA");
+        }
+        if (opc==2){
+            resultado= valor1-valor2;
+            JOptionPane.showMessageDialog(null,"O valor da subtração é: "+resultado+" RESULTADO DA SUBTRAÇÃO");
+        }
+         if (opc==3){
+            resultado= valor1/valor2;
+            JOptionPane.showMessageDialog(null,"O valor da divisão é: "+resultado+" RESULTADO DA DIVISÃO");
+        }
+          if (opc==4){
+            resultado= valor1*valor2;
+            JOptionPane.showMessageDialog(null,"O valor da multiplicação é: "+resultado+" RESULTADO DA MULTIPLICAÇÃO");
+        }
+    }
+    }
+    }
